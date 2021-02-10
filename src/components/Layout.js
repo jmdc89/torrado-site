fad8043633339648efd41e6fdd5f3662212b88d4
Jose Manuel <jmdcarq89@gmail.com>
2021-02-10 21:02:52 +0100

    sticky footer

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -3,15 +3,16 @@ import React from 'react'
 import Sidebar from '../components/Sidebar'
 import Footer from '../components/Footer'
 import '../styles/index.scss'
-import {container} from './layout.module.scss'
+import {container, content} from './layout.module.scss'
 
 function Layout(props) {
     return (
         <div className={container}>
+        <div className={content}>
             {props.children}
             <Sidebar/>
-            <Footer/>
-            
+        </div>  
+            <Footer/>         
         </div>
     )
 }
