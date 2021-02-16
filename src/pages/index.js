c497a55b57db4cad5a322605903979fac55c6dd6
Jose Manuel <jmdcarq89@gmail.com>
2021-02-16 11:29:48 +0100

    index mod

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,11 +2,12 @@ import React from "react"
 import {Link} from 'gatsby'
 import Footer from '../components/Footer'
 import Layout from '../components/Layout'
+import HomePage from "./home"
 
 const IndexPage = () => {
     return (
       <Layout>
-
+          <HomePage/>
       </Layout>
     )
 }
